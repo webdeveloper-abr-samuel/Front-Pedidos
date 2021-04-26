3af29733c224703e75e484122468b50ab9ddd074
TulioH <t.hernandez@abracol.com>
2021-04-26 12:50:30 -0500

    se corrige la url de la grafica statesDistributor

--- src/components/dashboard/Charts/StatesDistributor.vue
+++ src/components/dashboard/Charts/StatesDistributor.vue
@@ -8,57 +8,57 @@
 
 <script>
 import axios from 'axios'
-const URL = 'http://10.1.0.184:3000/abrageo'
-var Highcharts = require("highcharts");
+const URL = './abrageo'
+var Highcharts = require('highcharts')
 export default {
-  name: "StatesDistributor",
-  created() {
-    this.LoadData();
+  name: 'StatesDistributor',
+  created () {
+    this.LoadData()
   },
   methods: {
-    async LoadData() {
+    async LoadData () {
       const token = localStorage.getItem('Token')
       const config = {
         headers: { Authorization: `Bearer ${token}` },
       }
       try {
-        const result = await axios.get(`${URL}/statistic/distributor/States`, config);
+        const result = await axios.get(`${URL}/statistic/distributor/States`, config)
         await this.grafica(result.data.data)
       } catch (error) {
         console.log(error)
       }
     },
-    grafica(data) {
-        const Cantidad = []
-        const Departamento = []
-        data.forEach((element) => {
-            Cantidad.push(element.cantidad);
-            Departamento.push(element.departamento);
-        });
+    grafica (data) {
+      const Cantidad = []
+      const Departamento = []
+      data.forEach((element) => {
+        Cantidad.push(element.cantidad)
+        Departamento.push(element.departamento)
+      })
       const graficabar = document.getElementById('graficabarras')
       Highcharts.chart(graficabar, {
         chart: {
-          type: "column",
+          type: 'column',
         },
         title: {
-          text: "Cantidad de clientes por departamento",
+          text: 'Cantidad de clientes por departamento',
         },
         xAxis: {
           categories: Departamento,
         },
         yAxis: {
           title: {
-            text: "Cantidad",
+            text: 'Cantidad',
           },
         },
         series: [
           {
             name: Departamento,
             data: Cantidad,
-          }
+          },
         ],
-      });
+      })
     },
   },
-};
+}
 </script>
